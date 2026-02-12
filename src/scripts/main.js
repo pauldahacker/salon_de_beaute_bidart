97c413159dfbabad45998cf2d78432c058492b46
pauldahacker <paul.demasca@gmail.com>
2026-02-12 14:29:43 +0100

    layout change, script should work on netlify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,30 @@
 import AOS from "aos";
 
-AOS.init({
-  duration: 700,
-  once: true,
-  offset: 200
-});
+export function initSite() {
+    // AOS transitions
+    AOS.init({
+        duration: 700,
+        once: true,
+        offset: 200,
+    });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const menuButton = document.getElementById("menu-button");
-  const menuIcon = document.getElementById("menu-icon");
-  const mobileMenu = document.getElementById("mobile-menu");
+    // Menu Logic
+    const menuButton = document.getElementById("menu-button");
+    const menuIcon = document.getElementById("menu-icon");
+    const mobileMenu = document.getElementById("mobile-menu");
 
-  let isOpen = false;
-
-  menuButton?.addEventListener("click", () => {
-    isOpen = !isOpen;
-
-    if (isOpen) {
-      mobileMenu.classList.remove("hidden");
-      menuIcon.textContent = "✕";
-      document.documentElement.classList.add("overflow-hidden");
-    } else {
-      mobileMenu.classList.add("hidden");
-      menuIcon.textContent = "☰";
-      document.documentElement.classList.remove("overflow-hidden");
+    if (menuButton && mobileMenu) {
+        menuButton.addEventListener("click", () => {
+            const isOpened = !mobileMenu.classList.contains("hidden");
+            if (!isOpened) {
+                mobileMenu.classList.remove("hidden");
+                if (menuIcon) menuIcon.textContent = "✕";
+                document.documentElement.classList.add("overflow-hidden");
+            } else {
+                mobileMenu.classList.add("hidden");
+                if (menuIcon) menuIcon.textContent = "☰";
+                document.documentElement.classList.remove("overflow-hidden");
+            }
+        });
     }
-  });
-});
+}
